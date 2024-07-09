ac9d6973addbf13dd4f3c2b6f2889d52552eeeb2
Troy-nn <nrgnnrf@gmail.com>
2024-07-09 21:28:53 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+question = 'С какого ты города приехал? '
+my_string = input(question)
+print(len(question))
+print(my_string.upper())
+print(my_string.lower())
+print(question.replace(' ', ""))
+print(my_string[0])
+print(my_string[:6:-1])
